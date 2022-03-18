dd3d6b441ee334770dfd98aa3f4672cd9eae6e8b
ctdeakin <ctdeakin@gmail.com>
2022-03-18 09:33:22 -0400

    refactor: app is in seperate file to allow test runner to open connection to router without leaving open listen call

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const db = require('./server/db');
 const PORT = process.env.PORT || 8080;
-const app = express();
+const app = require('./app')
 
-module.exports={app, db}
 
 db.authenticate()
 .then(() => {
@@ -15,20 +14,5 @@ db.authenticate()
 
 db.sync().then(() => console.log('Database is synced'));
 
-
-
-// body parsing middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 'API' routes
-app.use('/api', require('./server/api'));
-
-// error handling endware
-app.use((err, req, res, next) =>
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-);
-
-app.get('/', (req, res) => res.send('Mira API'))
 
 app.listen(PORT, () => console.log(`Started on port ${PORT}`));
--- server.test.js
+++ server.test.js
@@ -1,7 +1,7 @@
-const {app, db} = require('./index');
+const app = require('./app')
 const request = require('supertest')
 
-describe('test the root path', () => {
+describe('test the root path', () => {  
   test('connection to api', async () => {
     const response = await request(app).get('/api/connect');
     expect(response.statusCode).toBe(200);
