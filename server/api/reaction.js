9396e7e6d93474c385933e62d52b6dfd12ea167d
Allan <allanwang97@gmail.com>
2022-03-23 11:32:51 -0400

    minor change

--- server/api/reaction.js
+++ server/api/reaction.js
@@ -1,20 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const User = require('../db/models/user')
-
-module.exports = router
-
-//sends all reactions
-router.get('/', async (req, res, next) => {
-  const users = await User.findAll()
-  res.send(users)
-})
-
-//send highest reactions
-router.get('/mostreactions', async (req, res, next) => {
-  const user = await User.max('reactions')
-  res.send(user)
-=======
 const { reaction } = require('../db/models')
 
 module.exports = router;
@@ -35,5 +19,4 @@ router.delete('/delete', async (req, res, next) => {
   }})
   cReaction[0].destroy()
   res.send('deleted')
->>>>>>> 10866b235bb7295584184783ba4bab07defe89fb
 })
