e894b0d022d53e7e88289b7315cd8e7bc735c827
Allan <allanwang97@gmail.com>
2022-03-25 14:37:41 -0400

    finished methods

--- utilities/singleUser.js
+++ utilities/singleUser.js
@@ -1,8 +1,11 @@
-const {tidy, count, filter} = require('@tidyjs/tidy')
+const {tidy, count} = require('@tidyjs/tidy')
 //these functions assemble data from raw sqlize query
 
 module.exports = {
-  topChannel
+  topChannel,
+  topWord,
+  topReaction,
+  hottestMessage
 }
 
 //finds the channel with the most messages for a user
@@ -10,7 +13,7 @@ module.exports = {
 // finds all channels associated with user, then the most common channelId
 //returns that channelID
 function topChannel(messages, userId) {
-  let userMessages = tidy(messages, filter((message) => message.userId = userId))
+  let userMessages = messages.filter(message => message.userId = userId)
   try {
     let channelCount = tidy(userMessages, count('channelId', {sort:true}))
     return channelCount[0].channelId
@@ -19,13 +22,48 @@ function topChannel(messages, userId) {
     throw new Error('top Channel Error')
   }
 }
-
 function topWord(messages, userId) {
-  let userMessages = tidy(messages, filter((message) => message.userId = userId))
-  try {
+  let userMessages = messages.filter(message => message.userId = userId)
+  let messageContent = userMessages.map(message => message.content)
 
-  } catch (error) {
-
+  let words = {}
+  for (let message in messageContent) {
+    for (let word in message) {
+      if (word in words)
+        words[word] += 1
+      else
+        words[word] = 1;
+    }
   }
+
+  let topWordCount = 0
+  let topWord = "asdf"
+  for (let [key, value] of Object.entries(words)) {
+    if (key.length > 2 && value > topWordCount) {
+      topWordCount = value
+      topWord = key;
+    }
+  }
+  return topWord
+}
+
+function topReaction(reactions, userId) {
+  let userReactions = reactions.filter(reaction => reaction.authorId = userId)
+  let reactionCount = tidy(userReactions, count('emojiId', {sort: true}))
+  return reactionCount[0].emojiId
+
+}
+
+function hottestMessage(reactions, messages, userId) {
+  let userReactions = reactions.filter(reaction => reaction.authorId = userId)
+  let reactionCount = tidy(userReactions, count('messageId', {sort: true}))
+
+  let hottestMessageId = reactionCount[0].hottestMessageId
+
+  for(let message of messages) {
+    if (message.id === hottestMessageId)
+      return message.content
+  }
+
 }
 
