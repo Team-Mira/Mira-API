d5dc6ef0d0bea665a57ea8140f2c965b7ef807ec
Robert Brown <robert.t.brown352@gmail.com>
2022-03-31 15:05:30 -0400

    Added controller for mentions

--- server/api/mention.js
+++ server/api/mention.js
@@ -1,18 +1,6 @@
 const router = require('express').Router();
-const { mention, message } = require('../db/models')
+const createMention = require('../controllers/mention/createMention')
 
 module.exports = router;
 
-router.post('/add', async (req, res, next) => {
-  const { newMention } = req.body
-
-
-  const cMessage = await message.findByPk(newMention.messageId)
-
-  if(!cMessage){
-    res.send('mention skipped')
-  }
-
-  await mention.create(newMention)
-  res.send('created')
-})
+router.post('/add', createMention)
