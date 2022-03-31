fd20eeb44380a86a2518ad84ef8863d1f6410b14
Robert Brown <robert.t.brown352@gmail.com>
2022-03-31 16:13:42 -0400

    Added controller for guilds

--- server/api/guilds.js
+++ server/api/guilds.js
@@ -1,16 +1,8 @@
 const router = require('express').Router();
-const guildValidation = require('../controllers/guildValidation')
 const getGuildIds = require('../controllers/guilds/getGuildIds')
 
 
 module.exports = router;
 
+// Gets all guild IDs for static paths
 router.get('/ids', getGuildIds)
-
-router.post('/access', async (req, res, next) => {
-  const { userId, guildId } = req.body
-
-  const hasAccess = await guildValidation(userId, guildId)
-
-  res.send(hasAccess)
-})
--- server/api/users.js
+++ server/api/users.js
@@ -5,6 +5,9 @@ const getUserIds = require('../controllers/users/getUserIds')
 
 module.exports = router;
 
+// Sends and array of objects with guild information and what channels the user
+// can see
 router.get('/:userId/guilds', getUserGuilds)
 
+// Gets all user IDs for static paths
 router.get('/ids', getUserIds)
