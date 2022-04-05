3c86b89f003f64db74dd43421359f0d17ec93b69
Robert Brown <robert.t.brown352@gmail.com>
2022-04-05 01:23:25 -0400

    tweaked reactions sort

--- utilities/users.js
+++ utilities/users.js
@@ -62,10 +62,10 @@ function mostUsedReaction(reactions) {
 
     const reactionSort = (a, b) => {
       if(a.value < b.value){
-        return -1
+        return 1
       }
       if(a.value > b.value){
-        return 1
+        return -1
       }
       return 0
     }
